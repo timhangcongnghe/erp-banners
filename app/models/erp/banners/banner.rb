30ed6d90700d5cb2a3ac3b7009a510bc80074f11
Ton Hung Nguyen <tonhungstar@gmail.com>
2018-08-08 09:10:10 +0000

    update global filter

--- app/models/erp/banners/banner.rb
+++ app/models/erp/banners/banner.rb
@@ -63,6 +63,14 @@ module Erp::Banners
 
       # join with categories table for search with category
       query = query.joins(:category)
+      
+      # global filter
+      global_filter = params[:global_filter]
+      if global_filter.present?
+				if global_filter[:category_id].present?
+					query = query.where(category_id: global_filter[:category_id])
+				end
+			end
 
       # join with users table for search creator
       query = query.joins(:creator)
